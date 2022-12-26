8dcb11d267e4b6aa0a0a8fde1edcac88d1f3db81
Lucas Rogerio da Silva <109930651+Lucas44794@users.noreply.github.com>
2022-12-26 12:14:43 -0300

    Alterei o ex98 e fiz + 1
    Fiz algumas alterações no ex098 e no ex098b fiz uma cópia da resposta do guanabara para ter as duas opções feitas

--- ex098.py
+++ ex098.py
@@ -1,28 +1,41 @@
+from time import sleep
+def linha():
+    print('-=' * 40)
+
 def contador(inicio, fim, passo):
-    for i in range(1, 11, 1):
-        print(f'{i}',end=' ')
-    print()
-    for i in range(10,-1,-1):
-        print(f'{i}',end=' ')
-    print()
     if passo != 0:
         if inicio < fim:
+            print(f'Contando de {inicio} a {fim} indo de {passo} em {passo}')
             for i in range(inicio, fim + 1, passo):
                 print(f'{i}',end=' ')
-        print()
+                sleep(0.5)
         if inicio > fim:
-            for i in range(fim, inicio+1, -passo):
-                print(f'{i}',end=' ')
+            if passo < 0:
+                print(f'Contando de {inicio} a {fim} indo de {passo} em {passo}')
+                for i in range(inicio, fim-1, passo):
+                    print(f'{i}',end=' ')
+                    sleep(0.5)
+            else:
+                print(f'Contando de {inicio} a {fim} indo de -{passo} em -{passo}')
+                for i in range(inicio, fim-1, -passo):
+                    print(f'{i}',end=' ')
+                    sleep(0.5)
         if inicio == fim:
             print('Valor incorreto inserido')
+        print('FIM')
     else:
         print('Passo 0 não existe!')
 
 
 
-print('~' * 30)
+print('~' * 40)
+contador(1, 10, 1)
+linha()
+contador(10, 0, 2)
+linha()
 ini = int(input('Inicio: '))
 fi = int(input('Fim: '))
 pas = int(input('Passo: '))
-print('~' * 30)
+print('~' * 40)
+sleep(1)
 contador(ini, fi, pas)
